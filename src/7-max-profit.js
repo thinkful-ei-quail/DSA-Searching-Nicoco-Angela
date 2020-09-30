1ab286e26715da7edf3441e33b8502f46708145f
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-30 14:36:51 -0700

    add more efficient max profit fn

--- src/7-max-profit.js
+++ src/7-max-profit.js
@@ -1,34 +1,3 @@
-const BinarySearchTree = require('./BinarySearchTree');
-
-const stocks = new BinarySearchTree();
-stocks.insert(128, 'Day 1: 128');
-stocks.insert(97, 'Day 2: 97');
-stocks.insert(121, 'Day 3: 121');
-stocks.insert(123, 'Day 4: 123');
-stocks.insert(98, 'Day 5: 98');
-stocks.insert(97, 'Day 6: 97');
-stocks.insert(105, 'Day 7: 105');
-
-// function sortPreOrder(inOrderArr, start = 0, end = inOrderArr.length, preOrderArr = []) {
-//   if (inOrderArr.length === 0) {
-//     return null;
-//   }
-//   let middleIndex = Math.floor((start + end) / 2);
-//   preOrderArr.push(inOrderArr[middleIndex]);
-//   let lesserArr = inOrderArr.slice(0, middleIndex);
-//   let greaterArr = inOrderArr.slice(middleIndex + 1, end);
-//   console.log('lesser', lesserArr);
-//   console.log('greater', greaterArr);
-// }
-// console.log(sortPreOrder([128, 97, 121, 123, 98, 97, 105]));
-// function createBinarySearchTree(arr) {
-//   const tree = new BinarySearchTree();
-//   for (let i = 0; i < arr.length; ++i) {
-//     tree.insert(arr[i], `Day ${i + 1}: ${arr[i]}`);
-//   }
-//   return tree;
-// }
-
 function findMaxProfit(arr) {
   let maxProfit = arr[1] - arr[0];
   while (arr.length) {
@@ -47,3 +16,19 @@ function findMaxProfit(arr) {
 console.log('expect 26: ', findMaxProfit([128, 97, 121, 123, 98, 97, 105]));
 console.log('expect 38: ', findMaxProfit([128, 97, 121, 123, 98, 97, 135]));
 console.log('expect 24: ', findMaxProfit([128, 97, 121, 100, 98, 97, 99]));
+
+function findMaxProfitEfficiently(arr) {
+  let buy = arr[0];
+  let maxProfit = 0;
+  for (let i = 1; i < arr.length; ++i) {
+    let sell = arr[i];
+    if (sell - buy < 0) {
+      buy = sell;
+    } else if (sell - buy > maxProfit) {
+      maxProfit = sell - buy;
+    }
+  }
+  return maxProfit;
+}
+
+console.log('expect 26: ', findMaxProfitEfficiently([128, 97, 121, 123, 98, 97, 105]));
