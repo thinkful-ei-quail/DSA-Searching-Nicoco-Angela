5023fbc92079ecfe11c98405fa28b4c364c96d49
Nicholas Hemerling <nicholas.hemerling@gmail.com>
2020-09-30 09:25:46 -0700

    add react app and linear search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  state = {
+    data: [89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5,],
+    response: null,
+    input: null,
+    error: null,
+  };
+
+  linearSearch(value) {
+    const { data } = this.state;
+    let searches = 0;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] == value) {
+        return `Value found at index ${i}, searched ${i + 1} time(s)`;
+      }
+      searches = i + 1;
+    }
+    return `Value not found, searched ${searches} time(s)`;
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      input: e.target.search.value
+    });
+    const { data } = this.state;
+    console.log(data);
+    const index = this.linearSearch(e.target.search.value);
+    const response = index;
+    this.setState({
+      response
+    });
+  };
+
+  render() {
+    return (
+      <main className='App' >
+        <h1>Searching</h1>
+        <form className="searchForm" onSubmit={this.handleSubmit}>
+          <label htmlFor="search">Linear Search Data For: </label>
+          <input type="number" id="search" name="search" />
+          <button type="submit">Search</button>
+        </form>
+        {(this.state.response || this.state.response === 0) &&
+          <section>
+            <h3>Index: {this.state.response}</h3>
+          </section>}
+      </main>
+    );
+  }
 }
 
 export default App;
